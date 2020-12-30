242c67a573e655f427a3473bcf0e004e858ded78
Boldeusz <mnpower@poczta.onet.pl>
2020-12-30 15:22:45 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -30,23 +30,34 @@ public class Main {
         highScorePosition = calculateHighScorePosition(50);
         displayHighScorePosition("Lucjan", highScorePosition);
     }
+
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table");
     }
+
     public static int calculateHighScorePosition(int playerScore) {
-        if (playerScore > 1000){
-            return 1;
-        }else if (playerScore > 500 && playerScore < 1000) {
-            return 2;
-        }else if (playerScore > 100 && playerScore < 500){
-            return 3;
-        }else {
-            return 4;
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//        return 4;
+        int position = 4;
+
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        }else if (playerScore >= 100) {
+            position = 3;
         }
+        return position;
     }
 
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus ) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
